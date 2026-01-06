3da2919911e6927c956976d040ed69df5cee5398
sayyadnasir31081980 <mohamednasir.10617443@ltimindtree.com>
2026-01-06 05:57:53 +0000

    Ninth commit

--- octofit-tracker/backend/tracker/urls.py
+++ octofit-tracker/backend/tracker/urls.py
@@ -4,7 +4,6 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .views import TeamViewSet, UserViewSet, ActivityViewSet, WorkoutViewSet, LeaderboardViewSet
-import os
 
 router = DefaultRouter()
 router.register(r'teams', TeamViewSet)
@@ -15,17 +14,12 @@ router.register(r'leaderboard', LeaderboardViewSet)
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    codespace_name = os.environ.get('CODESPACE_NAME', '')
-    if codespace_name:
-        base_url = f'https://{codespace_name}-8000.app.github.dev/api/'
-    else:
-        base_url = '/api/'
     return Response({
-        'teams': base_url + 'teams/',
-        'users': base_url + 'users/',
-        'activities': base_url + 'activities/',
-        'workouts': base_url + 'workouts/',
-        'leaderboard': base_url + 'leaderboard/',
+        'teams': '/api/teams/',
+        'users': '/api/users/',
+        'activities': '/api/activities/',
+        'workouts': '/api/workouts/',
+        'leaderboard': '/api/leaderboard/',
     })
 
 urlpatterns = [
